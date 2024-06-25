1c679691b5b46ece4bc001e41fa43d2ec81fae9e
Kartik Thakur <thakurkartik2262@gmail.com>
2024-06-25 12:24:49 +0530

    Deployment Fix

--- components/user-info.tsx
+++ components/user-info.tsx
@@ -1,4 +1,4 @@
-import { ExtendedUser } from "@/next-env";
+import { ExtendedUser } from "@/next-auth";
 import { Card, CardContent, CardHeader } from "./ui/card";
 import { Badge } from "./ui/badge";
 
